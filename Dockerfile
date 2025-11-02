27d3315ad73614b1d336b5e8013ddd2309df603c
kai <kirillsavenko1705@gmail.com>
2025-11-03 00:28:26 +0100

    added single docker compose files, .env for every service and fixed GatewayConfig for registration flow

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,17 @@ FROM maven:3.9.4-eclipse-temurin-21 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
-RUN mvn clean package
+RUN mvn clean package -DskipTests
 
 FROM eclipse-temurin:21-jre-alpine
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 
-EXPOSE 8080
+ENV SERVER_PORT=8080
+
+HEALTHCHECK --interval=30s --timeout=3s --start-period=10s --retries=3 \
+  CMD wget -qO- http://localhost:$SERVER_PORT/actuator/health || exit 1
+
+EXPOSE $SERVER_PORT
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/innowise/gateway/config/GatewayConfig.java
+++ src/main/java/com/innowise/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.innowise.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    @Value("${AUTH_SERVICE_URI}")
+    private String authServiceUri;
+
+    @Value("${USER_SERVICE_URI}")
+    private String userServiceUri;
+
+    @Value("${ORDER_SERVICE_URI}")
+    private String orderServiceUri;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("order-service", r -> r.path("/api/orders/**", "/api/items/**", "/api/order-items/**")
-                        .uri("http://localhost:8086"))
-                .route("user-service", r -> r.path("/api/users/**")
-                        .uri("http://localhost:8082"))
+                        .uri(orderServiceUri))
+                .route("user-service", r -> r.path("/api/users/**", "/api/cards/**")
+                        .uri(userServiceUri))
                 .route("auth-service", r -> r.path("/api/auth/**")
-                        .uri("http://localhost:8081"))
+                        .uri(authServiceUri))
                 .build();
     }
 }
